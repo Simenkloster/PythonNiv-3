1ca738adc366e04b988e8a22257a803d19c4919e
Emil1G <emil89587@gmail.com>
2025-02-24 19:22:17 +0100

    la til masse greier

--- assets/images/tiles/Gress.tsx
+++ assets/images/tiles/Gress.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.1" name="Gress" tilewidth="48" tileheight="48" tilecount="1" columns="1">
- <image source="../../../../../../../Downloads/Grass.png" width="48" height="48"/>
+ <image source="Grass.png" width="48" height="48"/>
 </tileset>
