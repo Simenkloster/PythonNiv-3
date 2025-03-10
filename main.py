3739c4721e81f9d49daca7be142c1fc1e36ed742
TheHonoredBacon456 <tubaobao777@hotmail.com>
2025-03-10 18:56:05 +0100

    legger til ting

--- main.py
+++ main.py
@@ -16,6 +16,31 @@ pg.display.set_caption("Tower Defense")
 #load images
 enemy_image = pg.image.load('assets/images/enemies/enemy_1.png').convert_alpha()
 
+
+
+
+
+
+
+
+
+
+#NYTT!!!
+#(#buttons
+buy_turret_image = pg.image.load('assets/images/buttons/buy_turret.png.png').convert_alpha()
+cancel_image = pg.image.load('assets/images/buttons/cancel.png')
+upgrade_turret_image = pg.image.load('assets/images/buttons/upgrade_turret.png').convert_alpha()
+
+#create buttons 
+turret_button = Button(c.SCREEN_WIDTH + 30, 120, buy_turret_image, True)
+cancel_button = Button(c.SCREEN_WIDTH + 50, 180, cancel_image, True)
+upgrade_button = Button(c.SCREEN_WIDTH + 5, 180, upgrade_turret_image, True)
+
+)
+
+
+
+
 #create groups
 enemy_group = pg.sprite.Group()
 turret_group = pg.sprite.Group()
@@ -55,16 +80,29 @@ while run:
                 run = False
             #mouse click
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-            mouse_pos = pg.mouse.get_pos() 
+                mouse_pos = pg.mouse.get_pos() 
             create_turret(mouse_pos)
             #check if mouse is on the game area
-            if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+            if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:                                                                
+                pass
+
+
             
 
     #update display
     pg.display.flip()
 
 pg.quit()
+
+
+
+
+
+
+
+#create buttons
+turret_button = Button(c.SCREEN_WIDTH + 30, 120, buy_turret_image, True)
+cancel_button = Button(c.SCREEN_WIDTH + 50, 180, cancel_image, True )
 
 
 
